4fdc91c17c3e9aa9bc75e761866fe1701e853980
Brecht2020 <60263223+Brecht2020@users.noreply.github.com>
2020-04-14 16:49:30 +0200

    Add files via upload

---
+++ CustomPasswordHasher.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestToDo.Web
+{
+    public class CustomPasswordHasher : IPasswordHasher
+
+    {
+
+        public string HashPassword(string password)
+
+        {
+
+            return Encrypt.GetMD5Hash(password);
+        }
+        public PasswordVerificationResult verifyHashedPassword( string hashedPassword, string providedPassword )
+
+        {
+
+            if (hashedPassword == HashPassword(providedPassword))
+                return PasswordVerificationResult.Success;
+            else
+                return PasswordVerificationResult.Failed;
+
+        }
+
+    }
+
+}
